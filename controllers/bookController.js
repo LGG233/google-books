e46c9510dfc62ad1e5606a85bfdeb21ff5d047f6
Fred Astaire <lancebc@thegodardgroup.com>
2019-06-19 18:50:59 -0400

    cleaned up controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,7 +4,7 @@ module.exports = {
     getBooksByTitle: function (req, res) {
         const { query: params } = req;
         axios
-            .get("https://www.googleapis.com/books/v1/volumes?q=jaws&key=AIzaSyBVDSPfCkUftiX_9sdX8UfXg_A_cQxPcFU", {
+            .get("https://www.googleapis.com/books/v1/volumes?q=jaws&key=" + APIKey, {
                 params
             })
             .then(function (restuls) {
